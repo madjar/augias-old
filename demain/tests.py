30f3293b8ad5718c9807cd909ce09933678e5b6e
Georges Dubus <georges.dubus@compiletoi.net>
2012-12-08 22:00:41 +0100

    Now generate a new assertion for the test if the cached one has expired.
    This code is now officially ugly as hell.

--- demain/tests.py
+++ demain/tests.py
@@ -16,14 +16,25 @@ def get_email_and_assertion(audience):
     if not _email_assertion:
         import pickle
         try:
-            _email_assertion = pickle.load(open(ASSERTION_CACHE_FILE, 'rb'))
-            # TODO : this may crash if the assertion is too old
+            import datetime
+            from browserid.utils import unbundle_certs_and_assertion, decode_json_bytes
+            # Get one from the cache, and check it's expiration
+            email_assertion = pickle.load(open(ASSERTION_CACHE_FILE, 'rb'))
+            _, assertion = unbundle_certs_and_assertion(email_assertion[1])
+            exp = decode_json_bytes(assertion.split('.')[1])['exp']
+            expiration_date = datetime.datetime.utcfromtimestamp(exp/1000)
+            if expiration_date < datetime.datetime.now():
+                # If it hasn't expired, return
+                _email_assertion = email_assertion
+                return email_assertion
         except FileNotFoundError:
-            import requests
-            from urllib.parse import quote_plus
-            r = requests.get('http://personatestuser.org/email_with_assertion/%s'%quote_plus(audience))
-            _email_assertion = r.json['email'], r.json['assertion']
-            pickle.dump(_email_assertion, open(ASSERTION_CACHE_FILE, 'wb'))
+            pass
+        # If no good one is available, get a new one
+        import requests
+        from urllib.parse import quote_plus
+        r = requests.get('http://personatestuser.org/email_with_assertion/%s'%quote_plus(audience))
+        _email_assertion = r.json['email'], r.json['assertion']
+        pickle.dump(_email_assertion, open(ASSERTION_CACHE_FILE, 'wb'))
     return _email_assertion
 
 
