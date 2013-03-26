2040b8193c3976a4385de9faa13bc14551003e3e
Georges Dubus <georges.dubus@compiletoi.net>
2013-03-26 12:39:18 +0100

    Removed the test dependencies from heroku.

--- augias/__init__.py
+++ augias/__init__.py
@@ -10,7 +10,6 @@ from .models import (
 
 from . import helpers
 
-@subscriber(BeforeRender)
 def add_global(event):
     event['h'] = helpers
 
@@ -29,10 +28,10 @@ def main(global_config, **settings):
 
     config.include('augias.utils.flash')
     config.add_request_method(get_user, 'user', reify=True)
+    config.add_subscriber(add_global, BeforeRender)
 
     config.set_default_permission('access')
 
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.scan()
+    config.scan('.views')
     return config.make_wsgi_app()
-
